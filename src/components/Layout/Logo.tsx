a39c4b866fdd990efd3cff463d676d2d8b27a633
denni582 <dennis.boute@gmail.com>
2022-01-20 20:13:17 +0100

    Updated navbar

--- src/components/Layout/Logo.tsx
+++ src/components/Layout/Logo.tsx
@@ -1,5 +1,5 @@
 import classes from './Logo.module.css';
-import logo from "../../assets/Logo.png";
+import logo from "../../assets/logo.png";
 
 const Logo = () => {
     return <div className={classes.div}><img className={classes.image} src={logo} alt='Little Momster'/></div>
--- src/components/Layout/MainNavigation.tsx
+++ src/components/Layout/MainNavigation.tsx
@@ -1,54 +1,53 @@
 import {NavLink} from 'react-router-dom';
 import classes from './MainNavigation.module.scss';
-import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
-import SmallLogo from "./SmallLogo";
+import {Container, Nav, Navbar, NavDropdown} from 'react-bootstrap';
+import SmallLogo from './SmallLogo';
 
 const MainNavigation = () => {
 
     return (
         <Navbar collapseOnSelect expand="lg" className={classes.navbar}>
-            <Container>
-                {/*<Navbar.Brand className={`${classes.brand} order-lg-0 ms-auto order-1`} href="/">*/}
-                <Navbar.Brand className={classes.brand} href="/">
-                    <SmallLogo />{' '}
+            <Container className={classes.container}>
+                <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
+                <Navbar.Brand className={classes.brand} href='/'>
+                    <SmallLogo/>{' '}
                     Little Momster
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                {/*<Navbar.Toggle aria-controls="responsive-navbar-nav" className={`${classes.toggle} order-lg-1 order-0`}/>*/}
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className='m-auto'>
-                        <Nav.Item>
-                            <Nav.Link eventKey="1">
-                                <NavLink to='/home' className={classes.link}>Home</NavLink>
-                            </Nav.Link>
-                        </Nav.Item>
-                        <Nav.Link>
-                            <NavDropdown title="Shop" id="basic-nav-dropdown">
-                                <NavDropdown.Item eventKey="2"><NavLink to='/products/speenkoorden' className={classes.dropdownItem}>
+                <Navbar.Collapse id='responsive-navbar-nav'>
+                    <Nav className={classes.center}>
+                        <Nav.Link eventKey='1'>
+                            <NavLink to='/home' className={classes.link}>Home</NavLink>
+                        </Nav.Link>
+                            <NavDropdown title='Shop' id='basic-nav-dropdown'>
+                                <NavDropdown.Item eventKey='2'><NavLink to='/products/speenkoorden'
+                                                                        className={classes.dropdownItem}>
                                     Speenkoorden
                                 </NavLink></NavDropdown.Item>
-                                <NavDropdown.Item eventKey="3"><NavLink to='/products/rammelaars' className={classes.dropdownItem}>
+                                <NavDropdown.Item eventKey='3'><NavLink to='/products/rammelaars'
+                                                                        className={classes.dropdownItem}>
                                     Rammelaars
                                 </NavLink></NavDropdown.Item>
-                                <NavDropdown.Item eventKey="4"><NavLink to='/products/speendoekjes' className={classes.dropdownItem}>
+                                <NavDropdown.Item eventKey='4'><NavLink to='/products/speendoekjes'
+                                                                        className={classes.dropdownItem}>
                                     Speendoekjes
                                 </NavLink></NavDropdown.Item>
-                                <NavDropdown.Item eventKey="5"><NavLink to='/products/dekentjes' className={classes.dropdownItem}>
+                                <NavDropdown.Item eventKey='5'><NavLink to='/products/dekentjes'
+                                                                        className={classes.dropdownItem}>
                                     Dekentjes
                                 </NavLink>
                                 </NavDropdown.Item>
-                                <NavDropdown.Item eventKey="6"><NavLink to='/products/nestjes' className={classes.dropdownItem}>
+                                <NavDropdown.Item eventKey='6'><NavLink to='/products/nestjes'
+                                                                        className={classes.dropdownItem}>
                                     Nestjes
                                 </NavLink></NavDropdown.Item>
                             </NavDropdown>
-                        </Nav.Link>
-                        <Nav.Link eventKey="7">
+                        <Nav.Link eventKey='7'>
                             <NavLink to='/contact' className={classes.link}>Contact</NavLink>
                         </Nav.Link>
-                        <Nav.Link eventKey="8">
+                        <Nav.Link eventKey='8'>
                             <NavLink to='/about-us' className={classes.link}>Over ons</NavLink>
                         </Nav.Link>
-                        <Nav.Link eventKey="9" className={classes.cart}>
+                        <Nav.Link eventKey='9' className={classes.cart}>
                             <NavLink to='/cart' className={classes.link}>Winkelwagen</NavLink>
                         </Nav.Link>
                     </Nav>
