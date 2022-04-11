23a5a08a7385149b33d98ffcea732c7707190291
denni582 <dennis.boute@tvh.com>
2022-04-11 19:48:03 +0200

    Added instafeed to home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import classes from './Home.module.css';
 import {Col, Container, Row} from "react-bootstrap";
 import {useTranslation} from "react-i18next";
-
+import InstagramFeed from "../../components/UI/instafeed";
 
 const Home = () => {
     const {t} = useTranslation('translation');
@@ -16,6 +16,8 @@ const Home = () => {
         <Row className={classes.row}>
             <Col xs={12} className={classes.col}>
                 {t('LITTLE_MOMSTER.HOME.TEXT')}
+                <InstagramFeed token={'IGQVJYRWhZAVi12ZA1hSMjdMdXpMUHczWnJsbVh2dG1RVXVrLWl6NW1HYmtfd1VscVR3RzZA6RmNHSVRvYzliUFpRVFdrdGMzbTNkMERkVUh0akFKeldldDdnRFZAzSWZA5QUdLTUZAEWi1nM1ZAFR25udkFJZAgZDZD'
+                }  counter="6"/>
             </Col>
         </Row>
     </Container>)
